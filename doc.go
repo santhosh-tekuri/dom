1743a1626ea45f1338dd454c36f08f0eb047aab8
Santhosh Kumar Tekuri <santhosh.tekuri@gmail.com>
2017-06-01 12:29:32 +0530

    add package doc

---
+++ doc.go
@@ -0,0 +1,12 @@
+// Copyright 2017 Santhosh Kumar Tekuri. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+/*
+Package dom provides document object model for xml.
+
+It does not strictly follow DOM interfaces, but has everything needed for
+xml processing library.
+
+*/
+package dom
